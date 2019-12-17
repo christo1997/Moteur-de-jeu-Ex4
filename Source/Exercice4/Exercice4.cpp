71efe27a9605b9cc3347f1da2d6141b194fe05a6
christo1997 <43686177+christo1997@users.noreply.github.com>
2019-12-17 16:44:16 -0500

    Add files via upload

---
+++ Source/Exercice4/Exercice4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Exercice4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Exercice4, "Exercice4" );
---
+++ Source/Exercice4/Exercice4GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Exercice4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXERCICE4_API AExercice4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
